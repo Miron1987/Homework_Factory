81955ef8ff8a0b90dc06ad63f7975647568cbbb8
Aleksandr Kougiya <zero0072002@mail.ru>
2017-10-31 18:40:08 +0300

    Factory

---
+++ HomeworkFactory/FactoryClass.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkFactory
+{
+    public abstract class FactoryClass
+    {
+
+        public BMW OrderBMW()
+        {
+            BMW bmw;
+            bmw = CreateBMW();
+            bmw.Prepare();
+            bmw.Body();
+            bmw.Engine();
+            bmw.Electric();
+            bmw.Constructing();
+            return bmw;
+        }
+
+        protected abstract BMW CreateBMW();
+    }
+
+    public class BMWSeriesOneFactory : FactoryClass
+    {
+        protected override BMW CreateBMW()
+        {
+            return new BMWSeriesOne();
+        }
+    }
+
+    public class BMWSeriesTwoFactory : FactoryClass
+    {
+        protected override BMW CreateBMW()
+        {
+            return new BMWSeriesTwo();
+        }
+    }
+
+
+    public abstract class BMW
+    {
+        public string Model { get; set; }
+        public string EngineCar { get; set; }
+        public string BodyCar { get; set; }
+
+        public void Prepare()
+        {
+            Console.WriteLine($"Готовимся к сборке модели {Model}");
+        }
+
+        public void Body()
+        {
+            Console.WriteLine($"Отдельно собираем кузов {BodyCar}");
+        }
+
+        public void Engine()
+        {
+            Console.WriteLine($"Собираем двигатель {EngineCar}");
+        }
+
+        public void Electric()
+        {
+            Console.WriteLine("Готовим электрику");
+        }
+
+        public void Constructing()
+        {
+            Console.WriteLine("Собираем все вместе");
+        }
+    }
+
+    public class BMWSeriesOne : BMW
+    {
+
+       public BMWSeriesOne(string model = "BMWSeriesOne", string engineCar = "8-цилиндровый W12", 
+           string bodyCar = "седан")
+        {
+            Model = model;
+            EngineCar = engineCar;
+            BodyCar = bodyCar;
+
+        }
+
+    }
+
+    public class BMWSeriesTwo : BMW
+    {
+
+        public BMWSeriesTwo(string model = "BMWSeriesTwo", string engineCar = "6-цилиндровый W8",
+            string bodyCar = "хэтчбек")
+        {
+            Model = model;
+            EngineCar = engineCar;
+            BodyCar = bodyCar;
+
+        }
+
+    }
+}
---
+++ HomeworkFactory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FactoryClass bmw1 = new BMWSeriesOneFactory();
+            FactoryClass bmw2 = new BMWSeriesTwoFactory();
+
+            BMW bmwCar = bmw1.OrderBMW();
+
+            Console.WriteLine();
+
+            bmwCar = bmw2.OrderBMW();
+
+
+
+        }
+    }
+}
